f7322bd7b9780fc4c95804c8fb3fa98e3356cf1c
84Ruk <81340433+84ruk@users.noreply.github.com>
2022-05-19 10:19:13 -0500

    Errores reparados

--- src/components/PokemonByType.jsx
+++ src/components/PokemonByType.jsx
@@ -4,7 +4,6 @@ import usePokemon from "../hooks/usePokemon";
 import { Pokemon } from "./Pokemon";
 import { toUpperCase } from "../helpers";
 import { Error } from "./Error";
-import InfiniteScroll from "react-infinite-scroll-component";
 import { Spinner } from "./Spinner";
 
 export const PokemonByType = () => {
@@ -20,24 +19,17 @@ export const PokemonByType = () => {
       {error ? <Error /> 
       : cargando ? <Spinner />
       :
-      <InfiniteScroll
-      dataLength={pokemon.length}
-      loader={<Spinner/>}
-      className="flex flex-wrap justify-center w-full px-10 max-w-screen-2xl mx-auto"
-      hasMore={false}
-    >
-      <h1 className="text-3xl font-semibold text-center my-3 w-full overscroll-contain">{filtroActual == 'Pokedex' ? null : 'Pokedex -'} {filtroActual}</h1>
-
-      {pokemon.map(pokemon => {
-        return (
+        <div>
+          <h1 className="text-3xl font-semibold text-center my-3 w-full overscroll-contain">{filtroActual == 'Pokedex' ? null : 'Pokedex -'} {filtroActual}</h1>
+          {pokemon.map(pokemon => (
             <Pokemon 
-              pokemon={pokemon}
-              key={pokemon.name} 
+              key={pokemon.id} 
+              pokemon={pokemon} 
             />
-      )})}
-
-    </InfiniteScroll>
+          ))}
+        </div>
       }
-    </div>
+
+      </div>
   )
 }
