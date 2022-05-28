5c6e8c13e3476e2112a0b6044b53cb59b3fa09c7
84Ruk <81340433+84ruk@users.noreply.github.com>
2022-05-28 09:08:41 -0500

    fix: errors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,25 @@
 import usePokemon from '../hooks/usePokemon';
 import { Search } from "./Search";
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
 export const Header = () => {
 
   const { setPokemon } = usePokemon();
+  const location = useLocation();
+
+  
+
 
   return (
     <>
-        <header className="mt-5 h-36  flex-col w-full max-w-screen-2xl flex items-center md:flex-row justify-between px-11 mx-auto">
-          <Link to={'/'}>
+        <header className="mt-5 h-28 flex-col w-full max-w-screen-2xl flex items-center md:flex-row justify-between px-11 mx-auto">
+          <Link to={location.pathname == '/' ? `null` :  `/`}>
             <img 
-              src='./src/assets/PokeLogo.png'
-              className='h-12 object-contain cursor-pointer'
+              src='../src/assets/PokeLogo.png'
+              className='h-10 object-contain cursor-pointer'
               alt='logo'
-              onClick={() => setPokemon([])}
+              onClick={() => 
+                location.pathname == '/' ? null :  setPokemon([])
+              }
 
               />
           </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['index.html', './src/**/*.jsx'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'bg-image': 'url("./assets/PokeLogo.png")',
+      }
+    },
   },
   plugins: [],
 }
